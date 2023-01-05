e4e88c0e205a4698f63e7f1cb80760804a155d2f
Владислав Алпатов <alpatof74@gmail.com>
2023-01-05 13:00:35 -0800

    try 2

--- src/srv/network/network.h
+++ src/srv/network/network.h
@@ -1,15 +1,17 @@
 #pragma once
 #include <string>
+#include <winsock2.h>
 #include <nlohmann/json.hpp>
 #include "../packets/Base.h"
 
 
 namespace Web::Network
 {
-    std::string RecvString(int soc);
-    nlohmann::json RecvJson(int soc);
-    void SendString(int soc, const std::string& str);
-    void SendJson(int soc, const nlohmann::json& jsn);
-	std::shared_ptr<Web::Packet::Base> RecvPacket(int soc);
+    std::string RecvString(SOCKET soc);
+    nlohmann::json RecvJson(SOCKET soc);
+
+    void SendString(SOCKET soc, const std::string& str);
+    void SendJson(SOCKET soc, const nlohmann::json& jsn);
+	std::shared_ptr<Web::Packet::Base> RecvPacket(SOCKET soc);
 }
 
