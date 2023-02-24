ed9947096c160d519060209f2c35129f5273603a
Vladislav <alpatof74@gmail.com>
2023-02-25 00:14:45 +0300

    removed namespace

--- src/srv/factories/PacketFactory.cpp
+++ src/srv/factories/PacketFactory.cpp
@@ -26,7 +26,7 @@
 #define PACKET_GET_USERINFO              3
 #define PACKET_SET_USER_STATUS           4
 #define PACKET_UPDATE_USER_CFG           5
-#define PACKET_GET_USER_CFGS             6
+#define PACKET_GET_USER_CONFIGS          6
 #define PACKET_SEND_CHAT_MESSAGE         7
 #define PACKET_CREATE_CHAT               8
 #define PACKET_DELETE_CHAT               9
@@ -35,27 +35,27 @@
 
 
 #define MAKE_DECORATED_PACKET(dec, packetType, data) std::make_unique<dec>(std::make_unique<packetType>(data));
+
+
 namespace Web
 {
     using namespace Packet;
-
+    using namespace Decorator;
     std::unique_ptr<BasePacket> PacketFactory::Create(const nlohmann::json &data)
     {
-        using namespace Packet;
-        
         switch (data["type"].get<int>())
         {
             case PACKET_AUTH:                   return std::make_unique<Auth>(data);
             case PACKET_ONLINE_USERS_COUNT:     return std::make_unique<OnlineUsersICount>(data);
-            case PACKET_SET_USERNAME:           return MAKE_DECORATED_PACKET(Decorator::RegisteredOnly, SetUserName,      data);
-            case PACKET_GET_USERINFO:           return MAKE_DECORATED_PACKET(Decorator::RegisteredOnly, GetUserInfo,      data);
-            case PACKET_SET_USER_STATUS:        return MAKE_DECORATED_PACKET(Decorator::RegisteredOnly, SetUserStatus,    data);
-            case PACKET_UPDATE_USER_CFG:        return MAKE_DECORATED_PACKET(Decorator::RegisteredOnly, UpdateUserConfig, data);
-            case PACKET_GET_USER_CFGS:          return MAKE_DECORATED_PACKET(Decorator::RegisteredOnly, GetUserConfigs,   data);
-            case PACKET_SEND_CHAT_MESSAGE:      return MAKE_DECORATED_PACKET(Decorator::RegisteredOnly, SendChatMessage,  data);
-            case PACKET_CREATE_CHAT:            return MAKE_DECORATED_PACKET(Decorator::RegisteredOnly, CreateChat,       data);
-            case PACKET_DELETE_CHAT:            return MAKE_DECORATED_PACKET(Decorator::RegisteredOnly, DeleteChat,       data);
-            case PACKET_DOWNLOAD_FILE:          return MAKE_DECORATED_PACKET(Decorator::RegisteredOnly, DownloadFile,     data);
+            case PACKET_SET_USERNAME:           return MAKE_DECORATED_PACKET(RegisteredOnly, SetUserName,      data);
+            case PACKET_GET_USERINFO:           return MAKE_DECORATED_PACKET(RegisteredOnly, GetUserInfo,      data);
+            case PACKET_SET_USER_STATUS:        return MAKE_DECORATED_PACKET(RegisteredOnly, SetUserStatus,    data);
+            case PACKET_UPDATE_USER_CFG:        return MAKE_DECORATED_PACKET(RegisteredOnly, UpdateUserConfig, data);
+            case PACKET_GET_USER_CONFIGS:       return MAKE_DECORATED_PACKET(RegisteredOnly, GetUserConfigs,   data);
+            case PACKET_SEND_CHAT_MESSAGE:      return MAKE_DECORATED_PACKET(RegisteredOnly, SendChatMessage,  data);
+            case PACKET_CREATE_CHAT:            return MAKE_DECORATED_PACKET(RegisteredOnly, CreateChat,       data);
+            case PACKET_DELETE_CHAT:            return MAKE_DECORATED_PACKET(RegisteredOnly, DeleteChat,       data);
+            case PACKET_DOWNLOAD_FILE:          return MAKE_DECORATED_PACKET(RegisteredOnly, DownloadFile,     data);
         }
         throw Exception::InvalidPacketType();
 
