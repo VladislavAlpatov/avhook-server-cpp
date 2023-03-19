70ef0cbc18090b765292304bae707f97c61788d5
Vladislav <alpatof74@gmail.com>
2023-03-19 03:46:26 -0700

    added clang tidy stuff

--- python-api/main.py
+++ python-api/main.py
@@ -3,6 +3,3 @@ from avs import *
 
 con = Connection("192.168.111.128", 7777, "1@mail.ru", "1235")
 user = con.get_logged_user()
-
-for chat in user.get_chats():
-    print(chat.get_name())
--- src/srv/network/exceptions.h
+++ src/srv/network/exceptions.h
@@ -4,15 +4,15 @@ namespace Web::Network::Exception
 {
     class RecvFailed final : public std::exception
     {
-        const char* what() const noexcept override
+        [[nodiscard]] const char* what() const noexcept override
         {
-            return "recv function faild";
+            return "recv function failed";
         }
 
     };
     class SocketAcceptionFaild final : public std::exception
     {
-        const char* what() const noexcept override
+        [[nodiscard]] const char* what() const noexcept override
         {
             return "Failed to accept new connection socket";
         }
